bf55bcb55e8ee281f084c03db14a794329826cc4
ErzhongHoo <1942218829@qq.com>
2021-11-01 12:38:12 +0800

    change touchpad name

--- touchpad.sh
+++ touchpad.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # 找到触摸板对应的id并赋予变量为id
-id=$(xinput list | grep 'ELAN2204:00 04F3:309A Touchpad' | awk '{print substr($6,4,5)}')
+#id=$(xinput list | grep 'ELAN2204:00 04F3:309A Touchpad' | awk '{print substr($6,4,5)}')
+id=$(xinput list | grep 'ETPS/2 Elantech Touchpad' | awk '{print substr($6,4,5)}')
 
 # 触控板对应的id,tap对应的数字
 tap=$(xinput list-props $id | grep "libinput Tapping Enabled (" | awk '{print substr($4,2,3)}')
